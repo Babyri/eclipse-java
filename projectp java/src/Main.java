5454eea814e50f31f3b4f8d811bc740db45822ab
Aarón <gonzalez3hernandezaaron@gmail.com>
2023-02-27 09:05:01 +0100

    push

---
+++ projectp java/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("hola mundo");
+		System.out.println("hola mundo");
+		System.out.println("hola mundo");
+		System.out.println("3*2");
+		System.out.println(3*2);
+		System.out.println("hola/    mundo"); 
+		System.out.printf("%d + %d + %d = %d", 5 , 6 , 7).println();
+
+
+	}
+
+}
